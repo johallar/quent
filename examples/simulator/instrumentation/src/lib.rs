8671101d0695b06898eea658a2816fdb9d1c5220
Joe O'Hallaron <johallaron@nvidia.com>
2026-08-12 15:50:08 -0600

    refactor(simulator): fix simulated nvtx ranges save to disk

--- examples/simulator/instrumentation/src/lib.rs
+++ examples/simulator/instrumentation/src/lib.rs
@@ -5,6 +5,8 @@
 
 use quent_model::{entity, instrumentation, model};
 
+#[cfg(feature = "collector")]
+pub mod collector_sink;
 pub mod nvtx;
 pub mod task;
 
@@ -16,6 +18,8 @@ entity! {
     Network: ResourceGroup {}
 }
 
+#[cfg(feature = "collector")]
+pub use collector_sink::SimulatorCollectorSink;
 pub use nvtx::{NvtxCapture, NvtxLayout, NvtxPushGuard, NvtxPushStack, NvtxStartGuard};
 pub use quent_query_engine_model::{engine, operator, plan, port, query, query_group, worker};
 pub use quent_stdlib::{channel, memory, processor};
